4479b953848facf0dd1920acf86f6cc9a275c244
Luis <luis.f.blanco@gmail.com>
2019-01-08 19:03:39 -0600

    Modified applicaton to use the Presentational pattern.

--- APM-Luis/src/app/products/product-edit/product-edit.component.ts
+++ APM-Luis/src/app/products/product-edit/product-edit.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /* NgRx */
 import { Store, select } from '@ngrx/store';
-import * as fromProduct from './../state/product.reducer';
+import * as fromProduct from './../state';
 import * as productActions from './../state/product.actions';
 
 import { Product } from '../product';
--- APM-Luis/src/app/products/product.module.ts
+++ APM-Luis/src/app/products/product.module.ts
@@ -7,8 +7,8 @@ import { reducer } from './state/product.reducer';
 
 import { SharedModule } from '../shared/shared.module';
 
-import { ProductShellComponent } from './product-shell/product-shell.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductShellComponent } from './containers/product-shell/product-shell.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
 /* NgRx */
